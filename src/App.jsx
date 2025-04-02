050c216628455a6a9ea4ffd14183929287c6b332
PIlotA330 <69285986+PIlotA330@users.noreply.github.com>
2025-04-01 22:26:34 -0500

    added more functionlaity to Board()

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   function handleClick(i) {
@@ -74,10 +74,13 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+  function handlePlay(nextSquares) {
+    // TODO
+  }
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
