6de2fba35227e0ca03c507fc30f64ed4cef41906
PIlotA330 <69285986+PIlotA330@users.noreply.github.com>
2025-04-01 22:42:10 -0500

    fixed Game()

--- src/App.jsx
+++ src/App.jsx
@@ -68,10 +68,10 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
-  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
