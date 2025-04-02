0486356c6d76a43e234d13815a5eb0da8e266d24
PIlotA330 <69285986+PIlotA330@users.noreply.github.com>
2025-04-01 21:32:47 -0500

    changed handleClick to setValue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Square({ value }) {
   const [value, setValue] = useState(null);
   function handleClick() {
-    console.log("clicked");
+    setValue("X");
   }
   return <button className="square" onClick={handleClick}>{value}</button>;
 }
@@ -12,19 +12,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   )
